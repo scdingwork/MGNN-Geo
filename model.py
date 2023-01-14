5c523d037b5a896228075bafd366ed0a5de5c3fe
lol <scdingwork@outlook.com>
2023-01-14 14:31:36 +0800

    Update model.py

--- model.py
+++ model.py
@@ -1,27 +1,15 @@
 from dgl.nn.pytorch import GraphConv,NNConv
 import torch
 import torch.nn as nn
-#D:\OneDrive - yourdrive\3_人生笔记\4 专业任务（长期笔记分类）\7_论文\1_blackhole_geo\6_延迟估计算法.md
-#延迟处理思路
-# black_geo_0121.py代码还要小改一下
+
 
 class MPNN(nn.Module):
     def __init__(self, aggregator_type,node_in_feats, node_hidden_dim, edge_input_dim, edge_hidden_dim,num_step_message_passing,gconv_dp,edge_dp,nn_dp1):
-        #原GCN是对基础的GraphConv(in_feats, hidden_size)的包装，多了一个linear_size参数，代表最后卷积层输出后，额外加一层的
-        # FC，好得到最终的表示，非常简单。。。
         super(MPNN, self).__init__()
         self.lin0 = nn.Linear(node_in_feats, node_hidden_dim)#65,32
-        self.num_step_message_passing=num_step_message_passing#层数 开始测试1层即可
+        self.num_step_message_passing=num_step_message_passing#
         edge_network = nn.Sequential(
-            # edge的处理网络 一般是MLP，输入自然是edge_input_dim，
-            # 最后的输出必须是图卷积层的节点输入特征x节点隐藏特征
-            # 原文注释 注意in_feats和out_feats就是NNConv(in_feats=node_in_feats,out_feats=node_hidden_dim,...)
-            # edge_func : callable activation function/layer
-            # Maps each edge feature to a vector of shape
-            # ``(in_feats * out_feats)`` as weight to compute messages.
-            #我们这里在原始的节点输入dim基础上，通过一层线性层将其转换为指定的维度作为图卷积层的输入node_hidden_dim
-            #参考qm9_nn 多层，edgefunc也是一样的 我的想法还没搞清楚；minist则是不一样的；mpnndgl也是一样的
-
+          
             nn.Linear(edge_input_dim, edge_hidden_dim),
             nn.ReLU(),
             nn.Dropout(p=edge_dp),
